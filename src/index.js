b4c34389d9e48a7bec591b11d50c9ec82ba7ebb6
Silica163 <96188282+Silica163@users.noreply.github.com>
2022-09-10 20:55:48 +0700

    Add wave form graph of mouse movement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 const canvas = document.getElementById("main");
-canvas.width = 500;
-canvas.height = 500;
 const mCtx = canvas.getContext("2d");
+const mxCtx = document.getElementById("mxgraph").getContext("2d");
+const myCtx = document.getElementById("mygraph").getContext("2d");
+var pos = 0
+function drawT(x,y){
+	mCtx.strokeStyle = "#fff";
+	mCtx.beginPath();
+	mCtx.moveTo(pos,mCtx.canvas.height/2);
+	mCtx.lineTo(pos,mCtx.canvas.height/2+y);
+	mCtx.stroke();pos++;
+	mCtx.clearRect(pos,0,1,500);
+	if(pos == 500){
+		pos=0;
+	}
+}
+
+function wavForm(ctx,x,y,id){
+	var pos = wavForm.pos[id ?? ctx.canvas.id]??0;
+	ctx.strokeStyle = "#fff";
+	ctx.beginPath();
+	ctx.moveTo(pos,ctx.canvas.height/2);
+	ctx.lineTo(pos,ctx.canvas.height/2+y);
+	ctx.stroke();
+	pos++;
+	ctx.fillStyle = "#0af";
+	ctx.fillRect(pos+1,0,1,ctx.canvas.height);
+	ctx.clearRect(pos,0,1,ctx.canvas.height);
+	if(pos == ctx.canvas.width){
+		pos=0;
+	}
+	wavForm.pos[ctx.canvas.id] = pos
+}
+wavForm.pos = {}
+
 const posData = {
 	mxe : document.getElementById("mx"),
 	mye : document.getElementById("my"),
@@ -21,6 +52,9 @@ const posData = {
 		posData.mye.innerHTML = posData.my;
 		posData.xe.innerHTML = posData.x;
 		posData.ye.innerHTML = posData.y;
+//		drawT(posData.x,posData.mx);
+		wavForm(mxCtx,0,posData.mx);
+		wavForm(myCtx,0,posData.my);
 	}
 }
 console.log(posData);
